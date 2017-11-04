62feeb6182f9a9d8572ea83c3030626c1ea27993
Swaroop <32168917+swarupe@users.noreply.github.com>
2017-11-04 19:13:31 +0530

    Added Second Layout.

--- app/src/main/java/com/example/donornation/LoginPageActivity.java
+++ app/src/main/java/com/example/donornation/LoginPageActivity.java
@@ -1,4 +1,33 @@
 package com.example.donornation;
 
-public class LoginPageActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        Button donar = (Button) findViewById(R.id.donar_button);
+        donar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LoginPageActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        Button seeker = (Button) findViewById(R.id.seeker_button);
+        seeker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LoginPageActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
 }
