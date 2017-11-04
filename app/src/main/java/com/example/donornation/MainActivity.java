af3dcd62ddbc4161f8b577ecc2c4a97cf77aafe8
Swaroop <32168917+swarupe@users.noreply.github.com>
2017-11-05 03:53:42 +0530

    Fully Changed 2.0

--- app/src/main/java/com/example/donornation/MainActivity.java
+++ app/src/main/java/com/example/donornation/MainActivity.java
@@ -5,18 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+      /*  Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            //show start activity
+
+            startActivity(new Intent(MainActivity.this, LoginPageActivity.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
+        */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button next = (Button) findViewById(R.id.next_activity_button);
+        Button next = (Button) findViewById(R.id.beadonor_button);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, LoginPageActivity.class);
+                Intent myIntent = new Intent(MainActivity.this, DonorRegistrationPageActivity.class);
                 startActivity(myIntent);
             }
         });
